32a06aa83618f06281b252134e92bfe93ab4abef
ghiegie <giancarlogarcia.gallano@gmail.com>
2023-11-12 22:38:51 +0800

    small adjustments for practice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello_world));
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn hello_world() -> &str {
+    let a = "hi";
+    a
 }
